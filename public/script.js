81a04410c38fc2ab987ee1d1c17b7b2c3f9089e2
Ignacio Bruno <ignaciobruno90@gmail.com>
2025-02-25 16:58:37 -0300

    Se agrega estatuto.json

--- public/script.js
+++ public/script.js
@@ -25,6 +25,16 @@ async function cargarDatosInfra() {
         console.error("Error al cargar los datos", error);
     }
 }
+
+async function cargarDatosEstatuto() {
+    try {
+        let response = await fetch("/api/getDataEstatuto.php");
+        let data = await response.json();
+        document.getElementById("resultado").textContent = JSON.stringify(data, null, 2);
+    } catch (error) {
+        console.error("Error al cargar los datos", error);
+    }
+}
 // async function cargarDatos() {
 //     try {
 //         let response = await fetch("/api/getData.php");
