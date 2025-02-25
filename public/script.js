f721aa9c48db64bfab1f0ebda93ab75652e35fd6
Ignacio Bruno <ignaciobruno90@gmail.com>
2025-02-25 15:20:46 -0300

    Probando modificaciones en la apo

--- public/script.js
+++ public/script.js
@@ -1,9 +1,36 @@
-async function cargarDatos() {
+async function cargarDatosHistoria() {
     try {
-        let response = await fetch("/api/getData.php");
+        let response = await fetch("/api/getDataHistoria.php");
         let data = await response.json();
         document.getElementById("resultado").textContent = JSON.stringify(data, null, 2);
     } catch (error) {
         console.error("Error al cargar los datos", error);
     }
 }
+async function cargarDatosDeportes() {
+    try {
+        let response = await fetch("/api/getDataDeportes.php");
+        let data = await response.json();
+        document.getElementById("resultado").textContent = JSON.stringify(data, null, 2);
+    } catch (error) {
+        console.error("Error al cargar los datos", error);
+    }
+}
+async function cargarDatosInfra() {
+    try {
+        let response = await fetch("/api/getDataInfra.php");
+        let data = await response.json();
+        document.getElementById("resultado").textContent = JSON.stringify(data, null, 2);
+    } catch (error) {
+        console.error("Error al cargar los datos", error);
+    }
+}
+// async function cargarDatos() {
+//     try {
+//         let response = await fetch("/api/getData.php");
+//         let data = await response.json();
+//         document.getElementById("resultado").textContent = JSON.stringify(data, null, 2);
+//     } catch (error) {
+//         console.error("Error al cargar los datos", error);
+//     }
+// }
